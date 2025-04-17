2f62c6e7bb367f88b59d997ca745bf89ff7795ad
sidritvejseli <sidrit2016@gmail.com>
2025-04-17 10:04:18 +0200

    iteration 1

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -2,74 +2,11 @@ package tdd;
 
 public class Game {
 
-	private int playerOneScore = 0;
-	private int playerTwoScore = 0;
-	
-	private boolean deuce = false;
-	
-	private boolean advantageP1 = false;
-	private boolean advantageP2 = false;
-	
-	public Object score() {
-		if(advantageP1 && advantageP2) {
-			return "Deuce";
-		}
-		
-		if(advantageP1) {
-			
-			return "Advantage Player 1";
-		}
-		
-		else if(advantageP2) {
-	
-			return "Advantage Player 2";
-		}
-		
-		
-		else if(deuce) {
-			
-			return "Deuce";
-			
-		}
-		
-		return this.playerOneScore+"-"+this.playerTwoScore;
+	public String score() {
+		// TODO Auto-generated method stub
+		return "0-0";
 	}
 
-	public void playerOneScores() {
-		if(this.playerOneScore<30) {
-			this.playerOneScore+=15;
-		}
-		else {
-			
-			if(deuce) {
-				advantageP1 =true;
-				deuce = false;
-			}
-			else if (playerTwoScore == 40) {
-				deuce = true;
-			}
-			
-			this.playerOneScore+=10;
-
-		}
-	}
-
-	public void playerTwoScores() {
-		if(this.playerTwoScore<30) {
-			this.playerTwoScore+=15;
-		}
-		else {
-}
-			if(deuce) {
-				advantageP2 =true;
-				deuce =false;
-			}
-			else if (playerOneScore == 40) {
-				deuce = true;
-				
-			this.playerTwoScore+=10;
-
-		}
-	}
+	
 
 }
